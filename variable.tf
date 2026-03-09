fb933ae248024b19bc8ec62120d7e546a1070b2a
avdhoot <avdhootlad4782@gmail.com>
2026-03-09 14:59:46 +0530

    commit

--- variable.tf
+++ variable.tf
@@ -6,7 +6,7 @@ variable "aws_region" {
 variable "ami_id" {
     description = "The AMI ID to use for the EC2 instance"
     type        = string
-    default     = "ami-0c55b159cbfafe1f0"
+    default     = "ami-0f559c3642608c138"
     }
 variable "instance_type" {
     description = "The type of EC2 instance to create"  
