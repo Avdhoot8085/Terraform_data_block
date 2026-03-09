cf1a78fb6d57c36837fcee27eb1e31029f15c39b
avdhoot <avdhootlad4782@gmail.com>
2026-03-09 15:31:08 +0530

    commit

--- main.tf
+++ main.tf
@@ -5,7 +5,8 @@ resource "aws_instance" "ec2" {
   ami           = var.ami_id
   instance_type = var.instance_type
   subnet_id = var.subnet_id
-  security_groups = [ data.aws_security_group.sg ]
+  security_groups = [ data.aws_security_group.sg.id]
+  key_name = [data.aws_key_pair.key]
     tags = {
         Name = "EC2-Instance"
     }
@@ -15,6 +16,6 @@ data "aws_security_group" "sg" {
   name = "my-sg"
   vpc_id = "vpc-01cc81afcbfb74343"
 }
-# data "aws_key_pair" "key_name" {
-#   key_name = "Avdhoot-key"
-# }
+data "aws_key_pair" "key" {
+  key_name = "Avdhoot-key"
+}
